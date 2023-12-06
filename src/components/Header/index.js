1f54a663af0d62176433fc228678edea1cf2c96a
LuccaDev <Luccafrietto1@hotmail.com>
2023-12-06 15:53:47 -0300

    estilizando o header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,25 @@
-function Header(){
+import logosvg from '../../svg/logo.svg';
+
+const Header = () =>{
+
+    const desclogo = "Essa é a logo do blog"
+
     return (
-        <h1>Header</h1>
+        <>
+            <header className='flex-space-between'>
+                <div className="logo">
+                    <img src={logosvg} alt={desclogo}></img>
+                </div>
+                <div className="search">
+                    <input type='text' class='input-search' name='search'></input>
+                </div>
+                <ul className="menu">
+                    <li> <a href="" className='nav-link'> Categorias</a> </li>
+                    <li> <a href="" className='nav-link'> Sobre</a> </li>
+                    <li> <a href="" className='nav-link'> Contato</a> </li>
+                </ul>
+            </header>  
+        </> 
     );
 }
 
